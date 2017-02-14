c345c4a51f3b0b4d761e3828ba0c392ea1f4b990
AlexandreLac <alexandrelac248@gmail.com>
2017-02-15 00:43:57 +0100

    add user security

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -5,19 +5,21 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Form\UserType;
+use AppBundle\Entity\User;
 
 class UserController extends Controller
 {
     /**
-     * @Route("/login", name="blog_login")
+     * @Route("/login", name="login")
      */
     public function loginAction(Request $request)
     {
-        return $this->render('AppBundle:user:login.html.twig');
+        return $this->render('AppBundle:security:login.html.twig');
     }
 
     /**
-     * @Route("/login_check", name="blog_check")
+     * @Route("/login_check", name="login_check")
      */
     public function loginCheckAction(Request $request)
     {
@@ -30,5 +32,34 @@ class UserController extends Controller
     public function adminAction()
     {
         return new Response('<html><body>Admin page!</body></html>');
+    }
+
+    /**
+     * @Route("/register", name="user_registration")
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $query = $em->createQuery("SELECT COUNT(u) FROM AppBundle:User u");
+            $usersCount = $query->getSingleScalarResult();
+
+            $role = ($usersCount == 0) ? 'admin' : 'user';
+            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+            $user->setRole($role);
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('articles');
+        }
+
+        return $this->render(
+            'AppBundle:Registration:register.html.twig',
+            array('form' => $form->createView())
+        );
     }
 }
